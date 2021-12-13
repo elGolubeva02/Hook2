de38409541306aa14b39a958ca688c4f1ab71814
Елизавета Голубева <elizabeththefirst2002@gmail.com>
2021-12-13 16:55:57 +0300

    итог

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace ConsoleApp3
 {
@@ -23,15 +25,16 @@ namespace ConsoleApp3
         static List<User> users = new List<User>();
         static void Main(string[] args)
         {
+            Size resolution = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
+            Console.WriteLine("Width: {0}, Height: {1}", resolution.Width, resolution.Height);
+            int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0;
+
             using (StreamReader sr = new StreamReader("X:/Doc.csv"))
             {
-                //цикл пока не достигли конца файла
                 while (sr.EndOfStream != true)
                 {
-                    //помещаем строку из файла в строковый массив по разделителю, принятому в csv
                     string[] str = sr.ReadLine().Split(';');
-                    //помещаем в элемент списка новую структуру типа User
-                    users.Add(new User() { x = Convert.ToInt32(str[0]), y = Convert.ToInt32(str[1]), time = Convert.ToDateTime(str[2])});
+                    users.Add(new User() { x = Convert.ToInt32(str[0]), y = Convert.ToInt32(str[1]), time = Convert.ToDateTime(str[2]) });
                 }
             }
             Console.Write("Введите начало временного промежутка ");
@@ -40,10 +43,72 @@ namespace ConsoleApp3
             DateTime k = Convert.ToDateTime(Console.ReadLine());
             foreach (User u in users)
             {
-                if(u.time > n && u.time < k)
+                if (u.time > n && u.time < k)
                 {
-                    Console.WriteLine(u);
+                    if (u.y < (resolution.Height) / 4)
+                    {
+                        count1 = count1 + 1;//отвечает за ворд
+                    }
+                    else if (u.x < (resolution.Width) / 4)
+                    {
+                        count2 = count2 + 1;//отвечает за соц сети
+                    }
+                    else if ((u.y > ((resolution.Height) / 4) * 2))
+                    {
+                        count3 = count3 + 1;//отвечает за мессенджеры
+                    }
+                    else if ((u.x > (resolution.Width) / 5 * 2) && (u.x < (resolution.Width) / 5 * 3) && (u.y > (resolution.Height) / 5 * 2) && (u.y < (resolution.Height) / 5 * 3))
+                    {
+                        count4 = count4 + 1;//отвечает за игры
+                    }
+                    else if ((u.y < (resolution.Width) / 4) && (u.y > (resolution.Width) / 4 * 3))
+                    {
+                        count5 = count5 + 1;//отвечает за БД
+                    }
                 }
+            }
+            int[] nums2 = new int[5] {count1, count2, count3, count4, count5};
+            Array.Sort(nums2);
+            if(nums2[4]==count1)
+            {
+                Console.WriteLine("Вероятно, сотрудник большинство времени работал в Word");
+            }
+            else if(nums2[4]==count2)
+            {
+                Console.WriteLine("Вероятно, сотрудник большинство времени провел в социальных сетях");
+            }
+            else if (nums2[4] == count2)
+            {
+                Console.WriteLine("Вероятно, сотрудник большинство времени провел в мессенджерах");
+            }
+            else if (nums2[4] == count3)
+            {
+                Console.WriteLine("Вероятно, сотрудник большинство времени провел за игрой");
+            }
+            else if (nums2[4] == count4)
+            {
+                Console.WriteLine("Вероятно, сотрудник большинство времени провел в программах Microsoft(БД/Excel)");
+            }
+
+            if (nums2[3] == count1)
+            {
+                Console.WriteLine("Но, возможно, мы ошиблись, и он работал в Word");
+            }
+            else if (nums2[3] == count2)
+            {
+                Console.WriteLine("Но, возможно, мы ошиблись, и он провел время в социальных сетях");
+            }
+            else if (nums2[3] == count2)
+            {
+                Console.WriteLine("Но, возможно, мы ошиблись, и он провел время в мессенджерах");
+            }
+            else if (nums2[3] == count3)
+            {
+                Console.WriteLine("Но, возможно, мы ошиблись, и он провел время за игрой");
+            }
+            else if (nums2[3] == count4)
+            {
+                Console.WriteLine("Но, возможно, мы ошиблись, и он провел время в программах Microsoft(БД/Excel)");
             }
             Console.ReadKey();
         }
